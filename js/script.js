3772a27f629ec34252640e1c5eaff11da9b13670
konpetrov <konstantinpetrovtf@gmail.com>
2022-03-14 04:07:03 +0300

    Commit entire project for the first time

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const logo = document.getElementById("logo");
+const headings = document.querySelectorAll("h2");
+const colors = ['#fbc531', '#1abc9c', '#00a8ff'];
+
+let i = 0;
+
+const changeColor = () => {
+    const color = colors[++i % colors.length]
+    logo.style.boxShadow = `5px 5px ${color}`;
+    headings.forEach(h => h.style.borderBottom = `5px solid ${color}`);
+    console.log('color changed')
+}
+
+logo.onmousedown = () => logo.style.boxShadow = 'none';
+logo.onmouseup = changeColor;
